7c20bae7af1b440969f5033a94917fc12b154964
Natalia Szwagierczak <nata.szwagierczak@gmail.com>
2021-12-22 20:59:52 +0100

    Add load from file mode for movement tracking

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from itertools import product
 from typing import List, Tuple, Dict
 from copy import deepcopy
 from time import sleep
+from os import path
 
 
 class Rewards:
@@ -202,25 +203,44 @@ def plot_results(results: List[int], category: str = 'Max') -> None:
     plt.show()
 
 
-def plot_movements(genetic_code: List[int] = None, grid: Grid = None, **kwargs):
+def plot_movements(genetic_code: List[int] = None, grid: Grid = None, grid_from_csv: bool = False, **kwargs) -> None:
     if not genetic_code:
-        genetic_code = np.genfromtxt('../docs/best_genetic_code.csv', delimiter=',', dtype=int)
+        genetic_code_path = '../docs/best_genetic_code.csv'
+        if path.isfile(genetic_code_path):
+            genetic_code = np.genfromtxt(genetic_code_path, delimiter=',', dtype=int)
+        else:
+            print('No genetic code. Creating new.')
+            genetic_code = list(np.random.choice(
+                len(Action.actions),
+                len(GeneticCode.direction_indices)))
     if not grid:
         grid = Grid(**kwargs)
+        if grid_from_csv:
+            grid_path = '../docs/grid.csv'
+            if path.isfile(grid_path):
+                grid.grid = np.genfromtxt(grid_path, delimiter=',', dtype=int)
+            else:
+                print('Cannot load grid from file. New grid created.')
 
     iterations = grid.get_movement_count()
-    print('\n' * 11)
+    total_points = 0
+    add_line_count = 4
+    print('\n' * (grid.length + add_line_count))
     for _ in range(iterations):
         grid_repr = np.where(grid.grid == 1, MovementRepr.can_repr[1], MovementRepr.can_repr[0])
         current_value = grid_repr[grid.current_position]
         grid_repr[grid.current_position] = MovementRepr.current_position_repr[current_value]
-        print('\033[11A')
+        print(f'\033[{grid.length + add_line_count}A')
         print(np.array2string(grid_repr, formatter={'str_kind': lambda x: x}, separator='  '))
         sleep(0.3)
         action = choose_action(
             grid_with_robot=grid,
             genetic_code=genetic_code)
-        _ = grid.move(action)
+        points_for_action = grid.move(action)
+        total_points += points_for_action
+        print()
+        print('Points for the action:\t', points_for_action)
+        print('Total points:\t\t', total_points)
 
 
 if __name__ == '__main__':
